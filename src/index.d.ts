56ab90430c30394d752bd569352d2cf2cbc96cac
Your Name <you@example.com>
2023-09-20 09:40:21 +0800

    rlr

--- src/index.d.ts
+++ src/index.d.ts
@@ -1,46 +1,46 @@
 import * as http from "http";
 import * as https from 'https';
-type PrimitiveType = string | boolean | number | bigint;
-type PromiseAble = Promise | void;
+export type PrimitiveType = string | boolean | number | bigint;
+export type PromiseAble = Promise<void> | void;
 
-interface PlainObject {
+export interface PlainObject {
     [key: string]: PrimitiveType;
 }
 
 
-interface InitHandler {
+export interface InitHandler {
     (app: App, globalData?: GlobalData): PromiseAble;
 }
 
 
 
-interface Context {
+export interface Context {
     res: http.ServerResponse,
     req: http.IncomingMessage,
     shareData: Object,
-    params(key?: String, dvalue?: any = null): void;
+    params(key?: String, dvalue?: any): void;
     paramsSet(key: String, value: any): void;
     success(data: any): void;
-    error(msg: String = "error", code: Number = 0, data: any = {}): void;
+    error(msg?: string, code?: Number, data?: any): void;
     html(html: String): void;
     content(content: String): void;
 }
-interface ContextHandler {
+export interface ContextHandler {
     (ctx: Context, globalData: GlobalData): PromiseAble;
 }
 
-interface GlobalData {
+export interface GlobalData {
     $service: Object | Function;
-    [key: any]: any
+    [key: string]: any
 }
 
-type Controller = Record;
+export type Controller = Record<string, ContextHandler>;
 
 
-declare class App {
+export declare class App {
     constructor();
     globalData: GlobalData;
-    module(name: String, modulePath: String):App;
+    module(name: String, modulePath: String): App;
     init(hook: InitHandler): App;
     use(handler: ContextHandler): App;
     service(name: String, service: Object): App;
@@ -48,8 +48,11 @@ declare class App {
     route(path: String, handler: ContextHandler): App;
     listen(port: Number, httpsOptions?: https.ServerOptions): Promise<http.Server>;
     callback(): Promise<http.RequestListener>
-    static createServer():App;
-    static defineController(controller:Controller):Controller;
+    static createServer(): App;
+    static defineController(controller: Controller): Controller;
 }
 
-export = App;
+export function defineController(controller: Controller): Controller;
+
+
+export default App;
